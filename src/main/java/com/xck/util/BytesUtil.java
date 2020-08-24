aaace67c6c840ed1c004c706f4fd17d7f7edf078
xck503c <2198494838@qq.com>
2020-08-24 18:58:51 +0800

    add test float to bit

--- src/main/java/com/xck/util/BytesUtil.java
+++ src/main/java/com/xck/util/BytesUtil.java
@@ -68,4 +68,62 @@ public class BytesUtil {
 
         return Double.longBitsToDouble(byteToLong(b)); //只能用这个来表示了
     }
+
+    /**
+     * 这里采用IEEE 754标准进行分解，用于验证class文件中存储的值
+     *
+     * 包含数符，尾数，阶码，尾数用补数，阶码用移码
+     * @param f
+     * @return
+     */
+    public static byte[] floatToByte(float f){
+
+        //1.确定数符
+        int sign = 0; //数符
+        if(f < 0) {
+            f = -f;
+            sign = 1;
+        }
+
+        String floatValue = (f+"");
+        int index = floatValue.indexOf(".");
+
+        String roudNumber = floatValue.substring(0, index);
+        String decimal = floatValue.substring(index+1);
+
+        StringBuilder roudSb = new StringBuilder("");
+        int tmpInt = Integer.parseInt(roudNumber);
+        while (true){
+            int bit = tmpInt%2;
+            tmpInt = tmpInt/2;
+            roudSb.append(bit ^ 1);
+            if(tmpInt == 0 || roudSb.length() == 23){
+                break;
+            }
+        }
+        roudSb.reverse();
+
+        StringBuilder decimalSb = new StringBuilder("");
+        float tmpFloat = Float.parseFloat(decimal);
+        while (true){
+            tmpFloat = tmpFloat*2;
+            int bit = tmpFloat > 1 ? 1 : 0;
+            if(tmpFloat > 1.0f){
+                bit = 1;
+                decimalSb.append(bit ^ 1);
+                tmpFloat = tmpFloat - 1;
+            }else if(tmpFloat == 1.0f){
+                decimalSb.append(bit ^ 1);
+                break;
+            }else {
+                decimalSb.append(bit ^ 1);
+            }
+            if(decimalSb.length() == 23){
+                break;
+            }
+        }
+
+        System.out.println(roudSb.toString());
+        return null;
+    }
 }
--- src/test/java/TestInt.java
+++ src/test/java/TestInt.java
@@ -1,5 +1,9 @@
 import com.xck.util.BytesUtil;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 /**
  * 测试整型的二级制表示
  * -10 - 11111111111111111111111111110110
@@ -7,10 +11,11 @@ import com.xck.util.BytesUtil;
 public class TestInt {
 
     public static void main(String[] args) {
-        intToBits();
+        floatToBits();
+        s();
     }
 
-    public static void intToBits(){
+    public static void byteToInt(){
         System.out.println(BytesUtil.byteToInt(new byte[]{0,44}));
     }
 
@@ -18,11 +23,52 @@ public class TestInt {
      * -128 - 10000000
      * -127 - 10000001
      */
-    public static void byteToBits(){
-        byte a = -127;
+    public static void byteToBits(byte a){
         for(byte i=0; i<8; i++){
             System.out.print(((0x80>>>i) & a) >>> (7-i));
         }
         System.out.println();
+    }
+
+    /**
+     *
+     * @param a
+     */
+    public static void intToBits(int a){
+        for(byte i=0; i<32; i++){
+            System.out.print(((0x80000000>>>i) & a) >>> (31-i));
+        }
+        System.out.println();
+    }
+
+    /**
+     * -127.45678f - -1023481377 - 1 1000010111111101110100111011111
+     */
+    public static void floatToBits(){
+//        float a = -127.45678f;
+        float a = 17.625f;
+        BytesUtil.floatToByte(a);
+        int b = Float.floatToIntBits(a);
+        System.out.println(b);
+        intToBits(b);
+    }
+
+    public static void s(){
+        String s = "00000010001011000100000";
+
+        float sum = 0.0f;
+        for(int i=0, j=1; i<s.length(); i++, j++){
+
+            int y = Integer.parseInt(s.charAt(i)+"");
+            if (y!=0) {
+                float x = 1;
+                for(int tmp=0; tmp<j; tmp++){
+                    x = x/2.0f;
+                }
+                sum = sum+y*x;
+                System.out.println(y*x);
+            }
+        }
+        System.out.println(sum);
     }
 }
